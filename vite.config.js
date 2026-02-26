756268ef628f5327dcc4f37ece245c15a90c0fad
SaadSenan1 <saaddodo609@gmail.com>
2026-02-27 00:26:01 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // مهم للنشر على أي سيرفر
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    target: 'esnext'
+  },
+  server: {
+    port: 5173
+  }
 })
